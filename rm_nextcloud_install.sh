3a8448be0f6568d31c42d0764b0384342c5f6e23
lanyaid <44942052+Lanyaid@users.noreply.github.com>
2021-01-29 12:23:32 +0100

    update rm_nextcloud_install.sh with new directories

--- rm_nextcloud_install.sh
+++ rm_nextcloud_install.sh
@@ -8,6 +8,8 @@ rm -r /mnt/system-cache/Nextcloud_conf/nextcloud/data/*
 rm -r /mnt/system-cache/Nextcloud_conf/nextcloud/data/.*
 rm -r /mnt/system-cache/Nextcloud_conf/nextcloud/themes/*
 rm -r /mnt/system-cache/Nextcloud_conf/nextcloud/themes/.*
+rm -r /mnt/system-cache/Nextcloud_conf/php-fpm.d/.*
+rm -r /mnt/system-cache/Nextcloud_conf/php-fpm.d/.*
 rm -r /mnt/system-cache/Nextcloud_conf/home_root/*
 rm -r /mnt/system-cache/Nextcloud_conf/nginx/*
 rm -r /mnt/system-cache/Nextcloud_conf/nginx/.*
